09bb40c59a73306f43e87e3f65f4a7890b198d98
John Sims <johnsims@Johns-Work-Computer.local>
2025-09-02 11:34:37 -0400

    Revert "add fetchAccessToken"
    This reverts commit 6bca1cc5e3043113c8dcc8818a46a8a706cf431e.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,8 @@ import { useAuth } from "@workos-inc/authkit-react";
 import { Link } from "react-router-dom";
 import { SignInButton } from "../components/sign-in-button";
 
-
 export default function Index() {
-  const { user, isLoading, getAccessToken } = useAuth();
-  // console.log('whole object', useAuth())
-  const fetchAccessToken = async () => {
-    const token = await getAccessToken();
-    console.log('Access Token:', token);
-  }
-
+  const { user, isLoading } = useAuth();
 
   if (isLoading) {
     return "..."
@@ -32,12 +25,6 @@ export default function Index() {
               <Link to="/account">View account</Link>
             </Button>
             <SignInButton large />
-            <Button asChild size="3" variant="soft" onClick={fetchAccessToken}>
-              <h1>Fetch Token</h1>
-            </Button>
-            <Button asChild size="3" variant="soft" onClick={fetchAccessToken}>
-              <h1>Fetch Token</h1>
-            </Button>
           </Flex>
         </>
       ) : (
